90eddbeafae1e2f9d2735eb06bcb101801a81f4c
HElhamaky <hassanelhamaky.social@gmail.com>
2021-06-13 14:21:37 +0200

    Initial Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c84544ad5f40116adc0f43ef847a0a27'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '4c8459fd-cd67-4b15-accf-dbe35093089c'
+};
